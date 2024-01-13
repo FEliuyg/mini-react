6b6ff4b31013916b6741e48b0f1ddcab129da1df
xiaoyao <15558110117@163.com>
2024-01-13 11:16:02 +0800

    feat: add vite to support jsx

--- App.js
+++
@@ -1,5 +0,0 @@
-import React from './core/react.js';
-
-const App = React.createElement('div', { className: 'App' }, 'Hello, React!');
-
-export default App;
---
+++ App.jsx
@@ -0,0 +1,5 @@
+import React from './core/react.js';
+
+const App = <div className='App'>Hello React</div>;
+
+export default App;
