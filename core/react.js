0ad6f67835edf4926e669bb8804b0d29931cbde4
xiaoyao <15558110117@163.com>
2024-01-14 22:03:05 +0800

    feat: support fiber

--- core/react.js
+++ core/react.js
@@ -21,17 +21,76 @@ function createTextElement(text) {
 }
 
 function render(element, parent) {
-  const { type, props } = element;
-  const dom = type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type);
+  const rootWork = {
+    dom: parent,
+    props: {
+      children: [element],
+    },
+  };
 
-  Object.keys(props).forEach((prop) => {
-    if (prop !== 'children') {
-      dom[prop] = props[prop];
+  nextUnitOfWork = performUnitOfWork(rootWork);
+}
+
+let nextUnitOfWork = null;
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (!shouldYield && nextUnitOfWork) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(work) {
+  // 1.创建dom
+  if (!work.dom) {
+    const dom = (work.dom =
+      work.type === 'TEXT_ELEMENT'
+        ? document.createTextNode('')
+        : document.createElement(work.type));
+
+    work.parent.dom.appendChild(dom);
+    // 2.更新props
+    Object.keys(work.props).forEach((prop) => {
+      if (prop !== 'children') {
+        dom[prop] = work.props[prop];
+      }
+    });
+  }
+
+  // 3.创建链表关系
+  let prev;
+  work.props.children.forEach((child, index) => {
+    let unitOfWork = {
+      type: child.type,
+      props: child.props,
+      dom: null,
+      child: null,
+      sibling: null,
+      parent: work,
+    };
+
+    if (index === 0) {
+      work.child = unitOfWork;
+    } else {
+      prev.sibling = unitOfWork;
     }
+    prev = unitOfWork;
   });
 
-  props.children.forEach((child) => render(child, dom));
-  parent.appendChild(dom);
+  // 4.返回下个要执行的任务
+  if (work.child) {
+    return work.child;
+  }
+
+  if (work.sibling) {
+    return work.sibling;
+  }
+
+  return work.parent?.sibling;
 }
 
 export default {
