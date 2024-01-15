76b2459139feb8e8e1fe118d227e3d662c2a09a9
xiaoyao <15558110117@163.com>
2024-01-15 21:10:07 +0800

    refactor: update function

--- core/react.js
+++ core/react.js
@@ -87,22 +87,31 @@ function initChildren(fiber, children) {
   });
 }
 
+function updateFunctionComponent(fiber) {
+  const children = [fiber.type(fiber.props)];
+  initChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber));
+
+    updateProps(dom, fiber.props);
+  }
+
+  const children = fiber.props.children;
+  initChildren(fiber, children);
+}
+
 function performUnitOfWork(fiber) {
   const isFunctionComponent = typeof fiber.type === 'function';
 
-  if (!isFunctionComponent) {
-    if (!fiber.dom) {
-      const dom = (fiber.dom = createDom(fiber));
-
-      updateProps(dom, fiber.props);
-    }
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
   }
 
-  // 3.创建链表关系
-  const children = isFunctionComponent ? [fiber.type(fiber.props)] : fiber.props.children;
-  initChildren(fiber, children);
-
-  // 4.返回下个要执行的任务
   if (fiber.child) {
     return fiber.child;
   }
